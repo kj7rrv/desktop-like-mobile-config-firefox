8624a27fd7d22ba620b223f94dc776e5e03ef675
Oliver Smith <ollieparanoid@postmarketos.org>
2020-08-10 16:48:54 +0200

    prefs.js: remove browser.uiCustomization.state
    This is done via userChrome.css now, which is easier to maintain (not
    all crammed into one JSON line).

--- src/prefs.js
+++ src/prefs.js
@@ -9,9 +9,6 @@ pref('dom.w3c.touch_events.enabled', true);
 pref('apz.allow_zooming', true);
 pref('apz.allow_double_tap_zooming', true);
 
-// Move all buttons to the overflow menu and remove spacers around the address bar
-pref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-fixed-list\":[\"stop-reload-button\",\"home-button\",\"library-button\",\"fxa-toolbar-menu-button\",\"sidebar-button\",\"downloads-button\"],\"nav-bar\":[\"back-button\",\"forward-button\",\"urlbar-container\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"PersonalToolbar\":[\"personal-bookmarks\"]},\"seen\":[\"developer-button\"],\"dirtyAreaCache\":[\"nav-bar\",\"toolbar-menubar\",\"TabsToolbar\",\"PersonalToolbar\",\"widget-overflow-fixed-list\"],\"currentVersion\":16,\"newElementCount\":4}");
-
 // Disable search suggestions
 pref('browser.search.suggest.enabled', false);
 
